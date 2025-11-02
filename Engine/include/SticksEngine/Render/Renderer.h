bf14f408cb6c2d2b6d988f2f1273847e7b6fcb25
Sticks <59712082+Sticks6110@users.noreply.github.com>
2025-11-01 23:04:48 -0400

    Initial commit

---
+++ Engine/include/SticksEngine/Render/Renderer.h
@@ -0,0 +1,12 @@
+//
+// Created by beast on 11/1/2025.
+//
+
+#ifndef STICKSENGINE2_RENDERER_H
+#define STICKSENGINE2_RENDERER_H
+class Renderer {
+public:
+    void printTest();
+};
+
+#endif //STICKSENGINE2_RENDERER_H
---
+++ Game/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <SticksEngine/Render/Renderer.h>
+
+int main() {
+    std::cout << "Hello, World! GAME" << std::endl;
+    Renderer renderer;
+    renderer.printTest();
+    return 0;
+}
